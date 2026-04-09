a280facb472f4cd708d49c19276f41e471a5b250
ntg-devs <giangnguyendev99@gmail.com>
2026-04-09 10:04:56 +0700

    Frontend 0.2v

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -34,6 +34,8 @@ module.exports = {
 
         card: 'var(--card)',
         'card-foreground': 'var(--card-foreground)',
+
+        'switch-background': 'var(--switch-background)',
       },
 
       borderRadius: {
@@ -46,6 +48,10 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+
+      animation: {
+        'pulse-slow': 'pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
       },
     },
   },
